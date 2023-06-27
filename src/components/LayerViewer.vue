5e63aef6612787bc99b46504362a3f3f2945bf80
Ollie Arrison <58272087+hermione-granger-maker@users.noreply.github.com>
2023-06-27 15:14:26 -0400

    Start view of layers
    Added a somewhat messy small rendering of a three js scene inside of cards

--- src/components/LayerViewer.vue
+++ src/components/LayerViewer.vue
@@ -4,32 +4,24 @@
         <v-slide-group
           v-model="model"
           class="pa-4"
-          selected-class="bg-success"
           show-arrows
         >
           <v-slide-group-item
-            v-for="n in 15"
+            v-for="n in 3"
             :key="n"
-            v-slot="{ isSelected, toggle, selectedClass }"
           >
-            <v-card
-              color="grey-lighten-1"
-              width="100"
-              height="100"
-              :class="['ma-4', selectedClass]"
-              @click="toggle"
-            >
-              <div class="d-flex fill-height align-center justify-center">
-                <v-scale-transition>
-                  <v-icon
-                    v-if="isSelected"
-                    color="white"
-                    size="48"
-                    icon="mdi-close-circle-outline"
-                  ></v-icon>
-                </v-scale-transition>
-              </div>
-            </v-card>
+            <div class="card-wrapper" ref="cardElements">
+              <v-card
+                width="100"
+                height="100"
+                class="transparent-card ma-4"
+              >
+                <div class="card-content">
+                  <div class="d-flex fill-height align-center justify-center">
+                  </div>
+                </div>
+              </v-card>
+            </div>
           </v-slide-group-item>
         </v-slide-group>
       </v-sheet>
@@ -37,11 +29,54 @@
   </template>
   
   <script>
+  import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+  
   export default {
-    data: () => ({
-      model: null,
-    }),
-  }
+    data() {
+      return {
+        model: null,
+      };
+    },
+    mounted() {
+      this.initializeScene();
+    },
+    methods: {
+      initializeScene() {
+        // Create a scene
+        const scene = new Scene();
+  
+        // Create a camera
+        const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
+        camera.position.z = 5;
+  
+        // Create a renderer
+        const renderer = new WebGLRenderer({ antialias: true });
+        const cardElements = this.$refs.cardElements;
+        const width = cardElements[0].offsetWidth;
+        const height = cardElements[0].offsetHeight;
+        renderer.setSize(width, height);
+        cardElements.forEach((cardElement) => {
+          const cardContentElement = cardElement.querySelector('.card-content');
+          cardContentElement.appendChild(renderer.domElement);
+        });
+  
+        // Create a cube
+        const geometry = new BoxGeometry();
+        const material = new MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new Mesh(geometry, material);
+        scene.add(cube);
+  
+        // Render the scene
+        function animate() {
+          requestAnimationFrame(animate);
+          cube.rotation.x += 0.01;
+          cube.rotation.y += 0.01;
+          renderer.render(scene, camera);
+        }
+        animate();
+      },
+    },
+  };
   </script>
   
   <style scoped>
@@ -56,6 +91,16 @@
     display: flex;
   }
   
+  .v-card {
+    background-color: transparent;
+    box-shadow: none;
+  }
+  
+  .transparent-card {
+    background-color: transparent;
+    border: none;
+  }
+
   .v-sheet {
     position: fixed;
     bottom: 0;
@@ -63,8 +108,16 @@
     color: rgb(176, 190, 197);
   }
   
-  .v-card {
-    color: rgb(176, 190, 197);
+  .card-wrapper {
+    display: inline-block;
+  }
+  
+  .card-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   </style>
   
